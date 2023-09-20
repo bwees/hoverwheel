a3468202fd3addc8c235ca23eff6cebaf44edb86
bwees <brandonwees@gmail.com>
2023-09-20 13:25:26 -0500

    initial firmware before test

---
+++ src/main.cpp
@@ -0,0 +1,167 @@
+#include <Arduino.h>
+#include <HoverboardAPI.h>
+#include <Wire.h>
+#include "MPU6050.h"
+#include "PIDController.h"
+
+int serialWrapper(unsigned char *data, int len) {
+ return (int) Serial2.write(data,len);
+}
+
+HoverboardAPI hoverboard = HoverboardAPI(serialWrapper);
+MPU6050 mpu;
+
+// enable debug mode
+// #define DEBUG
+
+#define FOOTPAD_SENSOR_THRESHOLD 600
+#define FOOTPAD_DEACTIVATE_DELAY 100
+
+#define MAX_PWM 600
+
+#define PUSHBACK_THRESHOLD .8
+#define PUSHBACK_TIME 2000
+#define PUSHBACK_SPEED 10 // degrees per second
+#define PUSHBACK_AMOUNT 15 // degrees
+
+// Board States
+#define STATE_IDLE 0
+#define STATE_RIDING 1
+#define STATE_PUSHBACK 2
+
+// PID Controller constants
+#define KP 50
+#define KI 0.005
+#define KD 400
+PIDController pid;
+
+void setup() {
+  Wire.begin();
+  // Wire.setClock(400000);
+  Serial2.begin(250000);
+
+  mpu.initialize();
+
+  if (!mpu.testConnection()) {
+    Serial2.println("MPU6050 connection failed");
+  }
+
+  pid.begin();
+  pid.tune(KP, KI, KD);
+
+  pinMode(PB4, OUTPUT);
+  pinMode(PC14, INPUT_PULLUP);
+  pid.limit(-600, 600);
+}
+
+float calc_pwm_command(int targetAngle, int board_tilt) {
+  return 0;
+}
+
+bool last_footpad = false;
+int last_footpad_change;
+int pushback_start_timer, pushback_end_timer;
+int state = STATE_IDLE;
+int microsTimer, loops;
+int targetAngle, pwm_cmd;
+
+int16_t ax, ay, az;
+float board_tilt;
+
+void loop() {
+  // Loop Performance Timer
+  long startMicros = micros();
+
+  //////////////// FOOTPADS //////////////// 
+  bool footpad = digitalRead(PC14);
+  bool footpad_change = footpad != last_footpad;
+
+  // record when the footpad last changed state
+  if (footpad_change) {
+    last_footpad_change = millis();
+  }
+
+  // if the footpad is released, and the board is not idle
+  if (!footpad && state != STATE_IDLE) {
+    // if the footpad has been released for more than FOOTPAD_DEACTIVATE_DELAY ms
+    if (millis() - last_footpad_change > FOOTPAD_DEACTIVATE_DELAY) {
+      state = STATE_IDLE;
+    }
+  } 
+
+  // if the footpad is pressed, and the board is idle
+  if (footpad && state == STATE_IDLE) {
+    state = STATE_RIDING;
+  }
+
+  //////////////// PUSHBACK ////////////////
+
+  // if PWM is above threshold 
+  if (abs(pwm_cmd) > MAX_PWM*PUSHBACK_THRESHOLD) {
+    pushback_end_timer = 0;
+
+    if (state == STATE_RIDING) {
+      // record when the PWM first went above threshold
+      if (pushback_start_timer == 0) {
+        pushback_start_timer = millis();
+      }
+
+      // for more than PUSHBACK_TIME ms, initiate pushback
+      if (millis() - pushback_start_timer > PUSHBACK_TIME) {
+        state = STATE_PUSHBACK;
+      }
+    }
+  } else if (abs(pwm_cmd) < MAX_PWM*PUSHBACK_THRESHOLD) {
+    pushback_start_timer = 0;
+
+    if (state == STATE_PUSHBACK) {
+      // record when the PWM first went above threshold
+      if (pushback_end_timer == 0) {
+        pushback_end_timer = millis();
+      }
+
+      // for more than PUSHBACK_TIME ms, end pushback
+      if (millis() - pushback_end_timer > PUSHBACK_TIME) {
+        state = STATE_RIDING;
+      }
+    }
+  }
+
+  // fade in and out of pushback 
+  if (state == STATE_PUSHBACK) {
+    // fade in pushback at PUSHBACK_SPEED deg/s to PUSHBACK_AMOUNT
+    int pushback_duration = millis() - pushback_start_timer - PUSHBACK_TIME;
+    targetAngle = constrain(pushback_duration*(PUSHBACK_SPEED/1000), 0, PUSHBACK_AMOUNT);
+  } else {
+    // fade out pushback at PUSHBACK_SPEED deg/s to 0
+    int end_pushback_duration = millis() - pushback_end_timer - PUSHBACK_TIME;
+    targetAngle = constrain(PUSHBACK_AMOUNT - end_pushback_duration*(PUSHBACK_SPEED/1000), 0, PUSHBACK_AMOUNT);
+  }
+  
+  ///////////// LEVELING LOGIC /////////////
+  // Get MPU Data
+  mpu.getAcceleration(&ax, &ay, &az);
+  board_tilt = (atan2(ay, az)*180.0)/M_PI;
+
+  // get PWM command
+  pid.setpoint(targetAngle);
+  int pid_out = pid.compute(board_tilt);
+
+  if (state != STATE_IDLE) {
+    pwm_cmd = pid_out;
+  } else {
+    // if the board is idle, set the PWM to 0
+    pwm_cmd = 0;
+  }
+
+  #ifndef DEBUG
+    hoverboard.sendDifferentialPWM(pwm_cmd, pwm_cmd, PROTOCOL_SOM_NOACK);
+  #else
+    // put data in Serial Plotter compatable format
+    Serial2.println("ANGLE:" + String(board_tilt) + ",TARGET:" + String(targetAngle) + ",PWM:" + String(pwm_cmd) + ",STATE:" + String(state));
+  #endif
+
+  // limit loop to 1000Hz
+  while (micros() - startMicros < 1000);
+  // hoverboard.sendDifferentialPWM(100, 30, PROTOCOL_SOM_NOACK);
+}
